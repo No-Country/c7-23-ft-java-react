3da9fd76acaddfa9524e3089b20f45150484f4cc
Jorge Tricarico <jorge.tricarico@gmail.com>
2022-10-16 17:54:43 -0300

    add more changues TurnosResponseMapper

---
+++ Back/MiTurno/src/main/java/com/miturno/models/dto/PatientResponse.java
@@ -0,0 +1,20 @@
+package com.miturno.models.dto;
+
+import com.miturno.models.Turn;
+import com.miturno.models.enums.DocumentType;
+
+import java.util.List;
+
+public class PatientResponse {
+
+    private Long idPatient;
+    private String name;
+    private String last_name;
+    private DocumentType documentType;
+    private Long document;
+    private String phone;
+    private String email;
+    private String social_work;
+    private String clinic_history;
+    private List<Long> idTurns;
+}
